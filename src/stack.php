0d61537c1c420ca8f4c32c834664b6f7ff560a85
Yoichi Izumi <quanyangyi0323@gmail.com>
2013-04-07 07:43:22 +0900

    add 2nd test

--- src/stack.php
+++ src/stack.php
@@ -1,8 +1,18 @@
 <?php
 
 class Stack {
+    private $value;
+
     public function isEmpty()
     {
         return true;
+    }
+    public function push($value)
+    {
+        $this->value = $value;
+    }
+    public function top()
+    {
+        return $this->value;
     }
 }
--- tests/StackTest.php
+++ tests/StackTest.php
@@ -2,8 +2,21 @@
 require_once(dirname(__FILE__) . "/../src/stack.php");
 
 class StackTest extends PHPUnit_Framework_TestCase {
-    public function testCreate() {
-        $stack = new Stack();
-        $this->assertTrue($stack->isEmpty());
+    protected $stack;
+
+    protected function setUp()
+    {
+        $this->stack = new Stack();
+    }
+
+    public function testCreate()
+    {
+        $this->assertTrue($this->stack->isEmpty());
+    }
+
+    public function testPushAndTop()
+    {
+        $this->stack->push(1);
+        $this->assertEquals(1, $this->stack->top());
     }
 }
